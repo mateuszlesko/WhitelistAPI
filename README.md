a3db8aafecb26735fb16fd1cf18c77ee5e3f6d62
mateuszlesko <mateusz.lesko@outlook.com>
2022-04-07 23:44:42 +0200

    backbone of API

---
+++ README.md
@@ -0,0 +1,3 @@
+# Credential API:
+
+The purpose of this API is getting access to the credential of IoT devices, that can send data to the main API.
--- server.go
+++ server.go
@@ -7,11 +7,34 @@ import (
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello there"))
+
+	if r.Method == "POST" {
+		w.Header().Set("Allow", "POST")
+		w.WriteHeader(405)
+		w.Write([]byte("Method not allowed"))
+		return
+	}
+}
+
+//send device id to get information if it is allow to send data
+func isAllowTo(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		id := r.URL.Query().Get("id")
+
+		w.Write([]byte(id))
+		return
+	}
+	if r.Method == "POST" {
+		w.Header().Set("Allow", "POST")
+		w.WriteHeader(405)
+		w.Write([]byte("Method not allowed"))
+	}
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/isallowto", isAllowTo)
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
