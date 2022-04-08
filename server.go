3ee6b7b16d10b16301bbae0310399bb78dcd52cd
mateuszlesko <mateusz.lesko@outlook.com>
2022-04-08 19:44:18 +0200

    white list creation

--- server.go
+++ server.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
+type Credential struct {
+	Code string `json:"Code"`
+}
+
 var credentialsArr [3]string
+
+const filePath string = "whitelist.data"
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello there"))
@@ -19,25 +30,62 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 //send device id to get information if it is allow to send data
-func isAllowTo(w http.ResponseWriter, r *http.Request) {
+func credentials(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		id := r.URL.Query().Get("id")
-		present := "no"
-		for _, x := range credentialsArr {
-			if x == id {
-				present = "yes"
+
+		var allow string = "no"
+
+		file, err := os.Open(filePath)
+		if err != nil {
+			log.Panic(err)
+		}
+
+		defer file.Close()
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			text := scanner.Text()
+			if text == id {
+				allow = "yes"
 				break
 			}
 		}
-
-		w.Write([]byte(present))
-		return
+		w.Write([]byte(allow))
 	}
-	if r.Method == "POST" {
-		w.Header().Set("Allow", "POST")
+
+	if r.Method == "PUT" {
+		w.Header().Set("Allow", "PUT")
 		w.WriteHeader(405)
 		w.Write([]byte("Method not allowed"))
 	}
+
+	if r.Method == "POST" {
+		var credential Credential
+		bodyBytes, err := io.ReadAll(r.Body)
+		json.Unmarshal([]byte(bodyBytes), &credential)
+		fmt.Printf("%s", &credential)
+		if err != nil {
+			w.WriteHeader(http.StatusConflict)
+			panic(err)
+
+		}
+		file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			w.WriteHeader(http.StatusConflict)
+			panic(err)
+		}
+
+		str := fmt.Sprint(credential.Code, "\n")
+		fmt.Printf(str)
+		fmt.Printf(str)
+		if _, err := file.WriteString(str); err != nil {
+			w.WriteHeader(http.StatusConflict)
+			panic(err)
+		}
+		defer file.Close()
+		w.WriteHeader(http.StatusCreated)
+	}
+	return
 }
 
 func main() {
@@ -45,7 +93,7 @@ func main() {
 	credentialsArr = [...]string{"0001", "0002", "0003"}
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
-	mux.HandleFunc("/isallowto", isAllowTo)
+	mux.HandleFunc("/whitelist", credentials)
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
